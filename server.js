a28db93cbcfb2847d0c8e6e233b12760ea050453
marmolpen3 <marmolpen3@alum.us.es>
2023-02-05 19:21:17 +0100

    config: Add database connection

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import http from "http";
 import express from "express";
 import { initialize } from "@oas-tools/core";
+import initMongoDBConnection from './config/mongoose.js'
 
 
 const deploy = async () => {
     const serverPort = 8080;
     const app = express();
-    app.use(express.json({limit: '50mb'}));
+    app.use(express.json({ limit: '50mb' }));
 
     const config = {
         middleware: {
@@ -17,22 +18,29 @@ const deploy = async () => {
         }
     }
 
-    
+    try {
+        await initMongoDBConnection()
+        console.log('Successfully connected to DB')
+    }
+    catch (err) {
+        console.error('Could not connect to DB ' + err)
+    }
+
     initialize(app, config).then(() => {
         http.createServer(app).listen(serverPort, () => {
-        console.log("\nApp running at http://localhost:" + serverPort);
-        console.log("________________________________________________________________");
-        if (config.middleware.swagger?.disable !== false) {
-            console.log('API docs (Swagger UI) available on http://localhost:' + serverPort + '/docs');
+            console.log("\nApp running at http://localhost:" + serverPort);
             console.log("________________________________________________________________");
-        }
+            if (config.middleware.swagger?.disable !== false) {
+                console.log('API docs (Swagger UI) available on http://localhost:' + serverPort + '/docs');
+                console.log("________________________________________________________________");
+            }
         });
     });
 }
 
 const undeploy = () => {
-  process.exit();
+    process.exit();
 };
 
-export default {deploy, undeploy}
+export default { deploy, undeploy }
 
