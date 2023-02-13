0288d660c944fde5a66b2e2c0725769bcaf973b2
marmolpen3 <marmolpen3@alum.us.es>
2023-02-13 12:34:18 +0100

    feat: Add some validation to Application entity

---
+++ api/controllers/validators/ApplicationValidator.js
@@ -0,0 +1,34 @@
+import { check } from "express-validator";
+
+const creationValidator = [
+  check("comment").exists({ checkNull: true, checkFalsy: true })
+    .isString()
+    .trim()
+    .escape(),
+  check("trip_id").exists({ checkNull: true, checkFalsy: true })
+    .isString()
+    .trim()
+    .escape()
+];
+
+const statusValidator = [
+  check("status").exists({ checkNull: true, checkFalsy: true })
+    .isString()
+    .isIn(["DUE", "ACCEPTED", "CANCELLED"])
+];
+
+const commentValidator = [
+  check("comment").exists({ checkNull: true, checkFalsy: true })
+    .isString()
+    .trim()
+    .escape()
+];
+
+const rejectValidator = [
+  check("rejectedReason").exists({ checkNull: true, checkFalsy: true })
+    .isString()
+    .trim()
+    .escape()
+];
+
+export { creationValidator, statusValidator, commentValidator, rejectValidator };
--- api/routes/ApplicationRoutes.js
+++ api/routes/ApplicationRoutes.js
@@ -7,22 +7,39 @@ import {
   updateApplicationComment,
   rejectApplication
 } from "../controllers/ApplicationController.js";
-// import handleExpressValidation from "../middlewares/ValidationHandlingMiddleware.js";
+import { creationValidator, statusValidator, commentValidator, rejectValidator } from '../controllers/validators/ApplicationValidator.js';
+import handleExpressValidation from '../middlewares/ValidationHandlingMiddleware.js';
 
 export default function (app) {
   app.route("/api/v1/applications")
     .get(getApplication)
-    .post(addApplication);
+    .post(
+      creationValidator,
+      handleExpressValidation,
+      addApplication
+    );
 
   app.route("/api/v1/applications/:id")
     .get(findById)
 
   app.route("/api/v1/applications/:id/change-status")
-    .patch(updateApplicationStatus);
+    .patch(
+      statusValidator,
+      handleExpressValidation,
+      updateApplicationStatus
+    );
 
   app.route("/api/v1/applications/:id/change-comment")
-    .patch(updateApplicationComment);
+    .patch(
+      commentValidator,
+      handleExpressValidation,
+      updateApplicationComment
+    );
 
   app.route("/api/v1/applications/:id/reject")
-    .patch(rejectApplication);
+    .patch(
+      rejectValidator,
+      handleExpressValidation,
+      rejectApplication
+    );
 }
